47ce0d17fdee1adb04cf841e38ff77bfc151f713
jmsweg <jt15435@my.bristol.ac.uk>
2019-02-19 19:46:28 +0000

    add some flags and skeleton

---
+++ denoiser/hyperparameters.py
@@ -0,0 +1,10 @@
+#The size of the image patches that will be fed into the network
+image_patch_dims = (65, 65, 3)
+
+mini_batch_size = 5
+
+learning_rate = 0.00001
+
+num_epochs = 5
+
+final_layer_size = 21
